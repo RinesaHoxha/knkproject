8a10e057ce4a6af6e8a68927b1ee97fff40fad28
rinesan <117470993+rinesan@users.noreply.github.com>
2023-05-21 17:02:44 +0200

    table item

--- src/main/java/com/example/knk/sql.sql
+++ src/main/java/com/example/knk/sql.sql
@@ -8,3 +8,23 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+--
+-- Table structure for table `Item`
+--
+
+DROP TABLE IF EXISTS `Item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `brand` varchar(30) DEFAULT NULL,
+  `price` int(10) DEFAULT NULL,
+  `imgUrl` varchar(100) DEFAULT NULL,
+  `stockAmount` int(10) DEFAULT NULL,
+  `soldAmount` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
