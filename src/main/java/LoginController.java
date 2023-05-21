94c271030c8f5fea93de7cc30dc7fbb99dbfe1e5
RinesaHoxha <rinesa.hoxha1@student.uni-pr.edu>
2023-05-21 02:22:27 +0200

    fajlli logincontroller

---
+++ src/main/java/LoginController.java
@@ -0,0 +1,28 @@
+package com.example.knk;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.stage.StageStyle;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import java.net.URL;
