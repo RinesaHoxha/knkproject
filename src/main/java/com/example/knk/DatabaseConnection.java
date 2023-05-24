2397f4bf335c9230e4d9d01eb28946c8e7856a75
Vesa Zhitia <118364175+vesax@users.noreply.github.com>
2023-05-24 14:39:24 +0200

    db

---
+++ src/main/java/com/example/knk/DatabaseConnection.java
@@ -0,0 +1,22 @@
+package com.example.knk;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DatabaseConnection {
+    public Connection conn;
+    public  Connection getConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn =  DriverManager.getConnection("jdbc:mysql://localhost/Ecom?user=black&password=tikur");
+
+
+        } catch (Exception ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("VendorError: " + ex.getCause());
+        }
+        System.out.println(conn);
+        return conn;
+    }
+}
