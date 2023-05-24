93ca2a2f18052674fb6fb645e9d3c40f6d9b8412
Vesa Zhitia <118364175+vesax@users.noreply.github.com>
2023-05-24 23:24:21 +0200

    connector

--- src/main/java/com/example/knk/DatabaseConnection.java
+++ src/main/java/com/example/knk/DatabaseConnection.java
@@ -3,12 +3,13 @@ package com.example.knk;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import com.mysql.cj.jdbc.Driver;
 public class DatabaseConnection {
     public Connection conn;
     public  Connection getConnection(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn =  DriverManager.getConnection("jdbc:mysql://localhost/Ecom?user=root&password=vesazhitia.12fr");
+            conn =  DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/knkproject?user=root&password=vesazhitia.12fr");
 
 
         } catch (Exception ex) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.example.knk {
     requires javafx.fxml;
     requires java.sql;
     requires javafx.graphics;
-
+    requires mysql.connector.java;
 
 
     opens com.example.knk to javafx.fxml;
